63abec7e2c5afdc9b765441bd24e4644f12d9934
Alexandre Rames <alexandre.rames@linaro.org>
2016-08-23 13:23:24 +0100

    AArch64: Avoid running the runtime call test for unsupported configurations.
    Change-Id: I2e0ce910fe5f5bb71188b62e7b5b4bdade8fb763

--- test/aarch64/test-assembler-aarch64.cc
+++ test/aarch64/test-assembler-aarch64.cc
@@ -22254,6 +22254,16 @@ void runtime_call_store_at_address(int64_t* address) {
 
 #ifdef VIXL_HAS_MACROASSEMBLER_RUNTIME_CALL_SUPPORT
 TEST(runtime_calls) {
+
+#ifndef VIXL_HAS_SIMULATED_RUNTIME_CALL_SUPPORT
+  if (masm.GenerateSimulatorCode()) {
+    // This configuration is unsupported and a `VIXL_UNREACHABLE()` would fire
+    // while trying to generate `CallRuntime`. This configuration should only be
+    // reachable with C++11 and a (buggy) version of GCC pre-4.9.1.
+    return;
+  }
+#endif
+
   SETUP();
 
   START();
