c059a35636cbacee1e1efc805a52af02ebfbeccd
Georgia Kouveli <georgia.kouveli@arm.com>
2017-05-12 14:54:56 +0100

    Remove Label versions of PLD and PLI from the MacroAssembler.
    These are unsafe to use, as we do not guarantee that the labels they refer to
    will be in range. Users can use the assembler version, or the MemOperand
    version instead.
    
    Change-Id: Ied575009f11514bbbca59a64f88e78cdc442fc73

--- test/aarch32/test-disasm-a32.cc
+++ test/aarch32/test-disasm-a32.cc
@@ -3448,11 +3448,8 @@ TEST(preloads) {
 
   COMPARE_A32(Pldw(MemOperand(r0, r1, LSL, 20)), "pldw [r0, r1, lsl #20]\n");
 
-  // PLD literal
-  Label pld_label;
-  COMPARE_A32(Pld(&pld_label);, "pld [pc, #-0]\n");
-  COMPARE_T32(Pld(&pld_label);, "pld [pc, #-0]\n");
-  __ Bind(&pld_label);
+  // PLD PC-relative
+  COMPARE_BOTH(Pld(MemOperand(pc, minus, 0)), "pld [pc, #-0]\n");
 
   // PLI immediate
   COMPARE_BOTH(Pli(MemOperand(r0, 0)), "pli [r0]\n");
@@ -3467,11 +3464,8 @@ TEST(preloads) {
 
   COMPARE_A32(Pli(MemOperand(r0, r1, LSL, 20)), "pli [r0, r1, lsl #20]\n");
 
-  // PLI literal
-  Label pli_label;
-  COMPARE_A32(Pli(&pli_label);, "pli [pc, #-0]\n");
-  COMPARE_T32(Pli(&pli_label);, "pli [pc, #-0]\n");
-  __ Bind(&pli_label);
+  // PLI PC-relative
+  COMPARE_BOTH(Pli(MemOperand(pc, minus, 0)), "pli [pc, #-0]\n");
 
   CLEANUP();
 }
